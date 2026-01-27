76cbfb1278922d9a85cb93ea7522239ce76bc1f0
Harish Ssv <harishsweet509@gmail.com>
2026-01-27 20:52:56 +0530

    print variable

--- print_variables.py
+++ print_variables.py
@@ -1,5 +1,22 @@
+#Type of Variable declaretions
 value = 5
 name = "Apple"
+x = y = z = "Hello, World!"
+a, b, c = "Apple","Orange","Python"
+
+fruits = ['apple','banana','cherry']
+u, v, w = fruits
 
 print(value) #print the variable value
 print(name) #print the variable value(string)
+
+#print the HelloWorld for these 3 variables
+print(x)
+print(y)
+print(z)
+
+#print the corresponding value assigned to the variables
+print(a)
+
+#print the value assaigned to the variables
+print(u)
