657ba539fb5ed23c39945a11b2adeccd15c5d0a9
Harish Ssv <harishsweet509@gmail.com>
2026-01-29 23:06:42 +0530

    String Manipulation

---
+++ String.py
@@ -0,0 +1,47 @@
+#1 The program should check if 'free' is in the text.
+txt = "The best things in life are free!"
+
+# Check if 'free' is in txt and print 'Yes'
+if "free" in txt:
+ print('Yes')
+
+#2 Leave this string as it is
+a = "Hello, World!"
+
+# Modify the print statement below 
+# to only print the second character of the string stored in a
+print(a[1])
+
+#3 Leave this string as it is
+a = "Hello, World!"
+
+# Modify the line below to print the string in lowercase letters
+print(a.lower())
+
+#4 Concatenate String
+a = "Hello"
+b = "World"
+
+# Combine the strings with a space between them, using the + operator
+c =a + " " + b
+
+# Leave the print as it is
+print(c)
+
+#5 Format Strings
+price = 59
+
+# Put the price inside the string using an f-string
+txt = f"The price is {price} dollars"
+
+# Printing the result
+print(txt)
+
+#6 Escape Characters
+# Use escape character to have "Vikings" in double quotes
+txt = "We are the so-called \"Vikings\" from the north."
+
+# Keep this print statement as it is
+print(txt) 
+
+#7 
