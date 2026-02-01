ba5848f68810c8533dd0489172c58298661dff73
Harish Ssv <harishsweet509@gmail.com>
2026-02-01 15:24:52 +0530

    Assigment Operators

---
+++ Assigment_Operators.py
@@ -0,0 +1,89 @@
+#Assigment Operators
+#Normal equal 
+x = 5
+
+print(x)
+
+#+ Equal
+x = 5
+
+x += 3
+
+print(x)
+
+#- Equal
+x = 5
+
+x -= 3
+
+print(x)
+
+#* Equal
+x = 5
+
+x *= 3
+
+print(x)
+
+#/ Equal
+x = 5
+
+x /= 3
+
+print(x)
+
+#% Equal
+x = 5
+
+x%=3
+
+print(x)
+
+#// Equal
+x = 5
+
+x//=3
+
+print(x)
+
+#** Equal
+x = 5
+
+x **= 3
+
+print(x)
+
+#& Equal
+x = 5
+
+x &= 3
+
+print(x)
+
+#^ Equal
+x = 5
+
+x ^= 3
+
+print(x)
+
+#>> Equal
+x = 5
+
+x >>= 3
+
+print(x)
+
+#<< Equal
+x = 5
+
+x <<= 3
+
+print(x)
+
+#: equal
+numbers = [1, 2, 3, 4, 5]
+
+if (count := len(numbers)) > 3:
+    print(f"List has {count} elements")
+
