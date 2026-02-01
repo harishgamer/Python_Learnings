3236676e082f812d47c9b0685faaf4436ba094f1
Harish Ssv <harishsweet509@gmail.com>
2026-02-01 17:32:39 +0530

    Introduction to List

---
+++ List_intro.py
@@ -0,0 +1,19 @@
+#List
+# Python has four built-in collection data types:
+# List, Tuple, Set, and Dictionary.
+#
+# These data types are used to store multiple values in a single variable.
+#
+# List:
+# - Uses square brackets []
+# - Allows duplicate values
+# - Mutable (values can be changed)
+# - Maintains insertion order
+# - Can store multiple data types
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+print(thislist)
+print(list1, "\n", list2, "\n", list3)
