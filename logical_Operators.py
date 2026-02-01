655c818e08c5137be835c594c6ed24f411e43cc6
Harish Ssv <harishsweet509@gmail.com>
2026-02-01 16:55:49 +0530

    Logical Operators

---
+++ logical_Operators.py
@@ -0,0 +1,15 @@
+#logical Operators
+#and Returns True if both statements are true
+x = 5
+
+print(x > 0 and x < 10)
+
+#or Returns True if one of the statements is true
+x = 5
+
+print(x < 5 or x > 10)
+
+#not Reverse the result, returns False if the result is true
+x = 5
+
+print(not(x > 3 and x < 10))
