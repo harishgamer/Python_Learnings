dc0306e2e9574e45b9e865c579b7bd3b40af78ac
Harish Ssv <harishsweet509@gmail.com>
2026-02-02 21:40:17 +0530

    Condition

---
+++ Condition.py
@@ -0,0 +1,48 @@
+#Condition
+#if Condition
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+#using bool datatype
+is_logged_in = True
+if is_logged_in:
+  print("Welcome back!")
+
+#elif
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+#multiple elif 
+score = 75
+
+if score >= 90:
+  print("Grade: A")
+elif score >= 80:
+  print("Grade: B")
+elif score >= 70:
+  print("Grade: C")
+elif score >= 60:
+  print("Grade: D")
+
+#else 
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+number = 7
+
+if number % 2 == 0:
+  print("The number is even")
+else:
+  print("The number is odd")
