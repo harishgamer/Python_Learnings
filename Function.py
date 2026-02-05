549dbe0e46f844aed560580706d7d7293138f4be
Harish Ssv <harishsweet509@gmail.com>
2026-02-05 21:41:06 +0530

    Function's

---
+++ Function.py
@@ -0,0 +1,13 @@
+#Function
+
+#Calling a function
+def my_function():
+ print("hello world")
+my_function()
+
+#fahrenheit_to_celsius
+
+def fahrenheit_to_celsius(fahrenheit):
+ return(fahrenheit - 32) * 5 / 9
+print(fahrenheit_to_celsius(59))
+print(fahrenheit_to_celsius(90))
