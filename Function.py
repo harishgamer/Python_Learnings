f0aa875688b6e54a33f5e243562552bcf1be8e8e
Harish Ssv <harishsweet509@gmail.com>
2026-02-06 19:45:37 +0530

    Function

--- Function.py
+++ Function.py
@@ -117,3 +117,23 @@ def myfunc1():
   return x
 
 print(myfunc1())
+
+#Lambda Functions
+#syntax: lambda arguments : expression
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 2))
+
+#using function
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11))
+print(mytripler(11))
+
+#Using Lambda with map()
+numbers = [1, 2, 3, 4, 5]
+doubled = list(map(lambda x: x * 2, numbers))
+print(doubled)
