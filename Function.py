ae186df174653d11812bde2d4671ef76758a33e0
Harish Ssv <harishsweet509@gmail.com>
2026-02-06 20:11:32 +0530

    Function and Recursion

--- Function.py
+++ Function.py
@@ -191,3 +191,14 @@ def sum_list(numbers):
 my_list = [1, 2, 3, 4, 5]
 print(sum_list(my_list))
 
+#example
+def find_max(numbers):
+  if len(numbers) == 1:
+    return numbers[0]
+  else:
+    max_of_rest = find_max(numbers[1:])
+    return numbers[0] if numbers[0] > max_of_rest else max_of_rest
+
+my_list = [3, 7, 2, 9, 1]
+print(find_max(my_list))
+
