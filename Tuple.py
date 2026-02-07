2a3dd79bfda6140c8fd4442aea84c431e4a81623
Harish Ssv <harishsweet509@gmail.com>
2026-02-07 17:35:11 +0530

    Tuple Handling's

--- Tuple.py
+++ Tuple.py
@@ -70,3 +70,27 @@ fruits = ("apple", "banana", "cherry")
 print(green)
 print(yellow)
 print(red)
+
+#using Asterisk* If the number of variables is less than the number of values, you can add an * to the variable name and the values will be assigned to the variable as a list
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+(green, yellow, *red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+#loop Tuple which is done with for and while loop 
+thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+  print(thistuple[i])
+
+thistuple = ("apple", "banana", "cherry")
+i = 0
+while i < len(thistuple):
+  print(thistuple[i])
+  i = i + 1
+
+
+
+
